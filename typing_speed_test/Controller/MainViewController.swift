f702f4466c6a9692dcf7451c7bb64c8f198ad28f
paul <kohleangpaul911@gmail.com>
2023-01-10 21:42:42 -0800

    one segue (if button press, it will go to other screen) added that connect  to main screen added

---
+++ typing_speed_test/Controller/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Main.swift
+//  typing_speed_test
+//
+//  Created by Paul on 2023-01-10.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
