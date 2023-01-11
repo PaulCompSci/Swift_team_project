73483fb1ee3fd25362747354316a23df4566c180
paul <kohleangpaul911@gmail.com>
2023-01-10 22:04:39 -0800

    segue from sign up page to log in page and to main page complete

--- typing_speed_test/Controller/ViewController.swift
+++ typing_speed_test/Controller/ViewController.swift
@@ -7,13 +7,16 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func SignUpButton(_ sender: UIButton) {
+        
+    }
+    
 }
 
